39bf85cc9fedf489cd537b40399bcdc287684827
Teshan2000 <teshan_wijewardhana@outlook.com>
2023-11-01 02:00:57 +0530

    Main Switch Case
    Switch case was added according to user choices.

--- Library.java
+++ Library.java
@@ -5,5 +5,28 @@ public class Library {
         System.out.println("------------------------------------------------------");
         System.out.println("|           === Welcome to the Library ===           |");
         System.out.println("------------------------------------------------------\n");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please Select your Choice:");
+        String choice = input.nextLine();
+
+        System.out.println("User");
+        System.out.println("Owner");
+        System.out.println("Exit");
+
+        switch(choice) {
+            case 1:
+            User();
+
+            case 2:
+            Owner();
+
+            case 3:
+            Exit();
+
+            case 4:
+            main(args);
+        }
     }
 }
