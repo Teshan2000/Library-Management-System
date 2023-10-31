fcb87168d232a4c3a649e1c91690cc13718d6290
Teshan2000 <teshan_wijewardhana@outlook.com>
2023-11-01 01:11:18 +0530

    Main Method
    Java class & main method was created.

---
+++ Library.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Library {
+    public static void main(String[] args) {
+        System.out.println("------------------------------------------------------");
+        System.out.println("|           === Welcome to the Library ===           |");
+        System.out.println("------------------------------------------------------\n");
+    }
+}
