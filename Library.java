448ec6eaa2ba7dc001b434d3eebfb7f23c6365df
Teshan2000 <teshan_wijewardhana@outlook.com>
2023-11-02 20:26:39 +0530

    User Actions
    In the user method, there are some actions to choose.

--- Library.java
+++ Library.java
@@ -29,4 +29,35 @@ public class Library {
             main(args);
         }
     }
+
+    static void User() {
+        System.out.println("------------------------------------------------");
+        System.out.println("\tWelcome User!");
+        System.out.println("------------------------------------------------");
+
+        System.out.println("Please Select your Choice:");
+        String choice = input.nextLine();
+
+        System.out.println("(1) Create an Account");
+        System.out.println("(2) Select Books");
+        System.out.println("(3) Borrow Books");
+        System.out.println("(4) Borrow Books");
+
+        switch(choice) {
+            case 1:
+            User();
+
+            case 2:
+            Owner();
+
+            case 3:
+            Exit();
+
+            case 4:
+            main(args);
+
+            case 5:
+            main(args);
+        }
+    }
 }
